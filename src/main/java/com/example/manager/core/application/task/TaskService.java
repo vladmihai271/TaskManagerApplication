2642298ae24bc673e7a39c7d71516ef21d7c4e9d
vladm <vladmihai271@gmail.com>
2023-12-05 19:57:08 +0200

    Added functionality to Tasks

--- src/main/java/com/example/manager/core/application/task/TaskService.java
+++ src/main/java/com/example/manager/core/application/task/TaskService.java
@@ -1,16 +1,22 @@
 package com.example.manager.core.application.task;
 
 import com.example.manager.core.application.repositories.EmployeeRepository;
+import com.example.manager.core.application.repositories.TaskRepository;
 import com.example.manager.core.domain.Employee;
 import com.example.manager.core.domain.Task;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TaskService {
     private final EmployeeRepository employeeRepository;
+    private final TaskRepository taskRepository;
 
-    public TaskService(EmployeeRepository employeeRepository) {
+    public TaskService(EmployeeRepository employeeRepository, TaskRepository taskRepository) {
         this.employeeRepository = employeeRepository;
+        this.taskRepository = taskRepository;
     }
 
     public void addTaskToEmployee(Task task){
@@ -20,5 +26,33 @@ public class TaskService {
         } else {
             employeeToTask.setTasks(employeeToTask.getTasks()+","+task.getTitle());
         }
+    }
+    public Optional<Long> findEmployeeByTaskId(Long taskId){
+        Optional<Task> task = taskRepository.findById(taskId);
+        if(task.isEmpty()){
+            return Optional.empty();
+        }
+        List<Employee> allEmployees = (List<Employee>) employeeRepository.findAll();
+        for(Employee employee : allEmployees){
+            if(employee.getTasks().contains(task.get().getTitle())){
+                return Optional.of(employee.getUid());
+            }
+        }
+        return Optional.empty();
+    }
+    public Optional<Employee> deleteTaskFromEmployee(Long taskId){
+        Optional<Long> employeeId = findEmployeeByTaskId(taskId);
+        Optional<Task> task = taskRepository.findById(taskId);
+        Optional<Employee> employee = Optional.empty();
+        if(employeeId.isPresent()) {
+            employee = employeeRepository.findById(employeeId.get());
+        }
+        if(employeeId.isPresent() && task.isPresent() && employee.isPresent()){
+            String employeeTasks = employee.get().getTasks();
+            employeeTasks = employeeTasks.replace("," + task.get().getTitle(),"");
+            employee.get().setTasks(employeeTasks);
+            return employee;
+        }
+        return Optional.empty();
     }
 }
