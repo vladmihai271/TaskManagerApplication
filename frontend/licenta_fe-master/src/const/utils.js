66da2ca87749a895a0fa7a5d891fc833a64b3525
vladm <vladmihai271@gmail.com>
2024-02-14 00:37:24 +0200

    Updated Frontend

---
+++ frontend/licenta_fe-master/src/const/utils.js
@@ -0,0 +1,25 @@
+import { SHA256 } from "crypto-js";
+
+export const setCredentialsToLocalStorage = (user, pass, id, rights) => {
+  localStorage.setItem("username", user);
+  localStorage.setItem("password", pass);
+  localStorage.setItem("userId", id);
+  localStorage.setItem("rights", rights);
+};
+
+export const getUserData = () => {
+  return {
+    username: localStorage.getItem("username"),
+    password: localStorage.getItem("password"),
+    userId: localStorage.getItem("userId"),
+    rights: localStorage.getItem("rights"),
+  };
+};
+
+export function hashPassword(password) {
+  // Hash the password using SHA-256
+  console.log("before", password);
+  const hashedPassword = SHA256(password).toString();
+  console.log("after", hashedPassword);
+  return hashedPassword;
+}
